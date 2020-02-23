1ea227e5d42d678ee58071e25856c632bcf3754c
Pooja Todkar <poojavk21@gmail.com>
2020-02-23 20:11:02 +0530

    modified server.js file

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.get('/getDetailsById', (req, res) => {
 	request.get(
